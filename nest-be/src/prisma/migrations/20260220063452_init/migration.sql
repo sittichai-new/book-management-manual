d8ae2a1b90baf64ad1c688a1567bce6ced01927f
Sittichai.th <Sittichai.th@vulcan.bluebik.com>
2026-02-20 15:07:58 +0700

    docker be

---
+++ nest-be/src/prisma/migrations/20260220063452_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Books" (
+    "book_id" SERIAL NOT NULL,
+    "book_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Books_pkey" PRIMARY KEY ("book_id")
+);
---
+++ next-fe/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold">Welcome to the Book Management App</h1>
+    );
+}
