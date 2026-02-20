513f42364b49bb3319529659a2f00194111b49c7
Sittichai.th <Sittichai.th@vulcan.bluebik.com>
2026-02-21 02:12:18 +0700

    fix docker

--- next-fe/app/page.tsx
+++ next-fe/app/page.tsx
@@ -1,4 +1,33 @@
+'use client';
+
+import { useState , useEffect } from 'react';
+
+async function fetchBooks() {
+  try {
+    const response = await fetch('http://localhost:4000/books/getBooks');
+    if (!response.ok) {
+      throw new Error('Failed to fetch books');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    return [];
+  }
+}
+
 export default function Home() {
+  const [books, setBooks] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchBooks().then((data) => {
+      setBooks(data);
+      setLoading(false);
+    });
+    console.log('Books fetched:', books);
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
         <h1 className="text-3xl font-bold text-center py-8">Books Page</h1>
